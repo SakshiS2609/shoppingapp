3551c2e999253d845d8574769c6393cfb21cce0a
SakshiS2609 <sakshi2692000@gmail.com>
2022-02-07 14:18:53 +0530

    updated

--- src/shoppingapp/main/Example.java
+++ src/shoppingapp/main/Example.java
@@ -1,5 +1,29 @@
 package shoppingapp.main;
 
-class Example {
 
-}
+
+	
+	 class SuperStudy 
+	{
+		public void X()
+		{ 
+			System.out.println("I am in SuperStudy.X()");
+			}
+
+		public static void main(String[] args) 
+		{ 
+			SuperStudy ssc = new SuperStudy();
+			ssc.X(); 
+		}
+	}
+
+	class SuperStudyChild extends SuperStudy
+	{
+		public void X()
+		{ 
+			super.X();
+			System.out.println("I am in SuperStudyChild.X()"); 
+		}
+	}
+
+
